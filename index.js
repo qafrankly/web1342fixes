195116e62d1ec21ba31a633fb5954b54054e1172
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-10-18 14:10:12 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+
+class RandomTest extends Component {
+  static PropTypes = {
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+  }
+  render(){
+    let {
+      h2 = 'Heba is testing react-websocket',
+    } = this.props;
+
+    let {
+      flux, config
+    } = this.context;
+
+    return (
+      <div>
+        <h2>{h2}</h2>
+      </div>
+    );
+  }
+}
+
+export default RandomTest;
